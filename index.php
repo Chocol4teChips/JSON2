a8cd5d968e8494ce6ba1897b10143d54d7e041de
ChocolateChips <74682378+Chocol4teChips@users.noreply.github.com>
2021-12-09 22:14:35 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <script src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <button id="btnBack"> back </button>
+
+    <div id="main">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Author</th>
+                </tr>
+            </thead>
+            <tbody id="tblPost">
+            </tbody>
+        </table>
+    </div>
+
+
+    <div id="detail">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>UserID</th>
+                </tr>
+            </thead>
+            <tbody id="tblDetails">
+            </tbody>
+        </table>
+
+        <div id="Comments">
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Detail</th>
+                    </tr>
+                </thead>
+                <tbody id="tblComments">
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
+</body>
+
+
+<script>
+
+    function showDetails(id) {
+        $("#main").hide();
+        $("#detail").show();
+
+        // console.log(id);
+        var url_post = "https://jsonplaceholder.typicode.com/posts/" + id
+        var url_comment = "https://jsonplaceholder.typicode.com/posts/" + id + "/comments"
+
+        $.getJSON(url_post)
+            .done((data) => {
+                console.log(data);
+                var line = "<tr id='detailROW'";
+                line += "><td>" + data.id + "</td>"
+                line += "<td><b>" + data.title + "</b><br/>"
+                line += data.body + "</td>"
+                line += "<td>" + data.userId + "</td>"
+                line += "</tr>";
+                $("#tblDetails").append(line);
+            })
+            .fail((xhr, err, status) => {
+
+            })
+        
+
+        $.getJSON(url_comment)
+            .done((data) => {
+                $.each(data,(k,item) => {
+                    console.log(data);
+                    var line = "<tr id = 'Comments'>";
+                        line += "<td>" + item.id + "</td>";
+                        line += "<td>" + item.name + "</td>";
+                        line += "<td><b>" + item.email + "</b></td>";
+                        line += "<td>" + item.body + "</td>";
+                        line += "</tr>";
+                    $("#tblComments").append(line);
+                });
+            })
+            .fail((xhr, error, status) =>  {
+
+            });
+
+    }
+
+    function LoadPosts() {
+        var url_post = "https://jsonplaceholder.typicode.com/posts"
+        $.getJSON(url_post)
+            .done((data) => {
+                $.each(data, (k, item) => {
+                    // console.log(item);
+                    var line = "<tr>";
+                    line += "<td>" + item.id + "</td>"
+                    line += "<td><b>" + item.title + "</b><br/>"
+                    line += item.body + "</td>"
+                    line += "<td><button onClick='showDetails(" + item.id + ");'>Link</button></td>"
+                    line += "</tr>";
+                    $("#tblPost").append(line);
+                });
+                $("#main").show();
+            })
+            .fail((xhr, err, status) => {
+
+            })
+    }
+
+    $(() => {
+        LoadPosts();
+        $("#detail").hide();
+        $("#btnBack").click(() => {
+            $("#main").show();
+            $("#detail").hide();
+            $("#detailROW").remove();
+        });
+    })
+</script>
+
+</html>
